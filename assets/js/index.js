0794de40eac46587aec6893d5e851d8ec66a9155
Juan <juanfernandoseguel@gmail.com>
2022-04-12 14:25:05 -0300

    asd

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,29 +3,37 @@ const vue = new Vue({
   data: function () {
     // datos
     return {
-      idUser: 0,
+      idUser: 1,
       text: null,
       chats: [
         {
           name: 'Sala juegos',
           image: 'https://placekitten.com/600/600',
+          createdAt: new Date().toLocaleString(),
         },
         {
           name: 'Sala adultos',
           image: 'https://placekitten.com/401/400',
+          createdAt: new Date().toLocaleString(),
         },
       ],
       messages: [
-        { text: 'asd', idUser: 0 },
+        { text: 'asd', idUser: 1 },
         { text: 'dfg', idUser: 2 },
         { text: 'fgh', idUser: 3 },
-        { text: 'oñwieyhr', idUser: 0 },
+        { text: 'oñwieyhr', idUser: 1 },
         { text: 'lsuieyhlushyfg', idUser: 3 },
       ],
     };
   },
   methods: {
-    //metodos
+    sendMessage() {
+      this.messages.push({
+        text: this.text,
+        idUser: this.idUser,
+      });
+      this.text = null;
+    },
   },
 });
 Vue.config.devtools = true;
